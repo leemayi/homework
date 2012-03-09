7bfbafb186789e20b8cbf0595e2c7acc771b0a99
huanghao <huang1hao@gmail.com>
2012-03-10 01:14:27 +0800

    遗传算法

--- gp.py
+++ gp.py
@@ -52,13 +52,11 @@ subw = fwrapper(lambda l:l[0]-l[1], 2, 'subtract')
 mulw = fwrapper(lambda l:l[0]*l[1], 2, 'multiply')
 
 def iffunc(l):
-    if l[0] > 0: return l[1]
-    else: return l[2]
+    return l[1] if l[0] > 0 else l[2]
 ifw = fwrapper(iffunc, 3, 'if')
 
 def isgreater(l):
-    if l[0] > l[1]: return 1
-    else: return 0
+    return 1 if l[0] > l[1] else 0
 gtw = fwrapper(isgreater, 2, 'isgreater')
 
 flist = [addw, mulw, ifw, gtw, subw]
@@ -77,13 +75,160 @@ def makerandomtree(pc, maxdepth=4, fpr=.5, ppr=.6):
             for i in range(f.childcount)]
         return node(f, children)
     elif random() < ppr:
-        return paramnode(randint(p, pc-1))
+        return paramnode(randint(0, pc-1))
     else:
         return constnode(randint(0, 10))
 
+def hiddenfunction(x, y):
+    return x**2 + 2*y + 3*x + 5
+
+def buildhiddenset():
+    rows = []
+    for i in range(200):
+        x = randint(0, 40)
+        y = randint(0, 40)
+        rows.append([x, y, hiddenfunction(x, y)])
+    return rows
+
+def scorefunction(tree, s):
+    dif = 0
+    for data in s:
+        v = tree.evaluate([data[0], data[1]])
+        dif += abs(v-data[2])
+    return dif
+
+def mutate(t, pc, probchange=.1):
+    if random() < probchange:
+        return makerandomtree(pc)
+    else:
+        result = deepcopy(t)
+        if isinstance(t, node):
+            result.children = [mutate(c, pc, probchange) for c in t.children]
+        return result
+
+def crossover(t1, t2, probswap=.7, top=1):
+    if random() < probswap and not top:
+        return deepcopy(t2)
+    else:
+        result = deepcopy(t1)
+        if isinstance(t1, node) and isinstance(t2, node):
+            result.children = [crossover(c, choice(t2.children), probswap, 0)
+                for c in t1.children]
+        return result
+
+def evolve(pc, popsize, rankfunction, maxgen=500, mutationrate=.1, breedingrate=.4, pexp=.7, pnew=.05):
+    def selectindex():
+        return int(log(random())/log(pexp))
+
+    population = [makerandomtree(pc) for i in range(popsize)]
+    for i in range(maxgen):
+        scores = rankfunction(population)
+        print scores[0][0]
+        if scores[0][0] == 0:
+            break
+        newpop = [scores[0][1], scores[1][1]]
+
+        while len(newpop) < popsize:
+            if random() > pnew:
+                newpop.append(mutate(crossover(
+                    scores[selectindex()][1],
+                    scores[selectindex()][1],
+                    probswap=breedingrate),
+                    pc, probchange=mutationrate))
+            else:
+                newpop.append(makerandomtree(pc))
+        population = newpop
+    scores[0][1].display()
+    return scores[0][1]
+
+def getrankfunction(dataset):
+    def rankfunction(population):
+        scores = [(scorefunction(t, dataset),t) for t in population]
+        scores.sort()
+        return scores
+    return rankfunction
+
+
+def main():
+    rf = getrankfunction(buildhiddenset())
+    evolve(2, 500, rf, mutationrate=.2, breedingrate=.1, pexp=.7, pnew=.1)
+
+
+
+
+
+def gridgame(p):
+    max = (3, 3)
+    lastmove = [-1, -1]
+    location = [[randint(0,max[0]), randint(0,max[1])]]
+    location.append([(location[0][0]+2)%4, (location[0][1]+2)%4])
+
+    for o in range(50):
+        for i in range(2):
+            locs = location[i][:] + location[1-i][:]
+            locs.append(lastmove[i])
+            move = p[i].evaluate(locs) % 4
+
+            if lastmove[i] == move:
+                return 1-i
+
+            lastmove[i] = move
+            if move == 0:
+                location[i][0] -= 1
+                if location[i][0] < 0: location[i][0] = 0
+            if move == 1:
+                location[i][0] += 1
+                if location[i][1] > max[0]: location[i][0] = max[0]
+            if move == 2:
+                location[i][1] -= 1
+                if location[i][1] < 0: location[i][1] = 0
+            if move == 3:
+                location[i][1] += 1
+                if location[i][1] > max[1]: location[i][1] = max[1]
+
+            if location[i] == location[1-i]: return i
+    return -1
+
+def tournament(pl):
+    losses = [0 for p in pl]
+
+    for i in range(len(pl)):
+        for j in range(len(pl)):
+            if i == j: continue
+
+            winner = gridgame([pl[i], pl[j]])
+            if winner == 0:
+                losses[j] += 2
+            elif winner == 1:
+                losses[i] += 2
+            elif winner == -1:
+                losses[i] += 1
+                losses[j] += 1
+    z = zip(losses, pl)
+    z.sort()
+    return z
+
+class humanplayer(object):
+    def evaluate(self, board):
+        me = tuple(board[:2])
+        others = [tuple(board[x:x+2]) for x in range(2, len(board)-1, 2)]
+
+        for i in range(4):
+            for j in range(4):
+                if (i,j) == me:
+                    print 'O',
+                elif (i,j) in others:
+                    print 'X',
+                else:
+                    print '.',
+            print
+        print 'Your last move was %d' % board[len(board)-1]
+        print ' 0'
+        print '2 3'
+        print ' 1'
+        print 'Enter move: ',
+        move = int(raw_input())
+        return move
 
 if __name__ == '__main__':
-    t = exampletree()
-    print t.evaluate([2, 3])
-    print t.evaluate([5, 3])
-    t.display()
+    main()
