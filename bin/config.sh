2e1f955edff0ad94c77cd6357d487a5d0bd309e5
huanghao <huang1hao@gmail.com>
2012-08-29 00:49:42 +0800

    - setup env - move quiz1 to ex1/

---
+++ bin/config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# *************************************************
+# config.sh
+# -------------------
+# This should be sourced in the profile of a sh,
+# bash, ksh, or zsh shell.
+# Last edited: September 18, 2011
+# *************************************************
+
+if [ "$ALGS4_CONFIG_SOURCED" != "true" ]; then
+
+    base=$HOME/workspace/algo-class
+
+	# Standard libraries
+	export CLASSPATH=$CLASSPATH:$base/stdlib.jar:$base/algs4.jar
+	
+	# Checkstyle and Findbugs
+	export PATH=$PATH:$base/bin
+	
+	# Making sure these variables aren't doubly set
+	export ALGS4_CONFIG_SOURCED=true
+	
+fi
