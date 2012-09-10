ab01314be6fc9565f240bed4c68d80060abb94c2
Huang Hao <hao.h.huang@intel.com>
2012-09-10 21:16:56 +0800

    Implement Subset.java

---
+++ ex2/Subset.java
@@ -0,0 +1,16 @@
+
+public class Subset {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+
+        RandomizedQueue<String> q = new RandomizedQueue<String>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            q.enqueue(item);
+        }
+
+        for (int i=0; i<k && !q.isEmpty(); ++i)
+            StdOut.printf("%s\n", q.dequeue());
+    }
+}
