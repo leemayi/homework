d5f08825f73c88317238de1e80fa9d504d36a814
huanghao <huang1hao@gmail.com>
2012-11-07 08:30:54 +0800

    2/3 of ex2

---
+++ CfDA/rankall.R
@@ -0,0 +1,26 @@
+rankall <- function(outcome_param, num = "best") {
+    outcome <- read.csv("outcome-of-care-measures.csv", colClasses="character", na.strings="Not Available")
+
+    HEART_ATTACK <- 11
+    HEART_FAILURE <- 17
+    PNEUMONIA <- 23
+    HOSPITAL_NAME <- 2
+
+    if (outcome_param == "heart attack") {
+        col = HEART_ATTACK
+    } else if (outcome_param == "heart failure") {
+        col = HEART_FAILURE
+    } else if (outcome_param == "pneumonia") {
+        col = PNEUMONIA
+    } else {
+        stop("invalid outcome")
+    }
+
+    rank <- function(group) {
+        group[1]
+    }
+
+    groups = split(outcome, outcome$State)
+    lapply(groups, rank)
+}
+
--- CfDA/rankhospital.R
+++ CfDA/rankhospital.R
@@ -1,5 +1,4 @@
-best <- function(state, outcome_param) {
-    ## Read outcome data
+rankhospital <- function(state, outcome_param, num = "best") {
     outcome <- read.csv("outcome-of-care-measures.csv", colClasses="character", na.strings="Not Available")
 
     HEART_ATTACK <- 11
@@ -30,11 +29,25 @@ best <- function(state, outcome_param) {
     }
     data <- data1[valid.idx,]
 
-    i = sort(data[,col], index.return=T)$ix[1]
-    data[i, HOSPITAL_NAME]
+    rank = sort(data[,col], index.return=T)$ix
+    if (num == "best") {
+        i = 1
+    } else if (num == "worst") {
+        i = length(rank)
+    } else if (!is.numeric(num) || num < 1 || num > length(rank)) {
+        i = -1
+    } else {
+        i = num
+    }
 
+    if (i == -1) {
+        "NA"
+    } else {
+        data[rank[i], HOSPITAL_NAME]
+    }
+    ## Read outcome data
     ## Check that state and outcome are valid
-
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
+    ## Return hospital name in that state with the given rank
+    ## 30-day death rate
 }
+
